13ed27de2960b1e77515e78af8ca489611412d16
phtchung <phamthanhchung2001@gmail.com>
2023-04-28 16:35:07 +0700

    update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,20 @@
 <template>
-  <div >
-    <el-menu
-      :default-active="activeIndex2"
-      mode="horizontal"
-      class="el-menu-demo menu"
-      @select="handleSelect"
-      background-color="#000000"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-      <el-menu-item index="1">Home</el-menu-item>
-      <el-menu-item>
-      <el-avatar  src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
-      </el-menu-item>
-
-<!--      <el-menu-item :span="18">-->
-<!--        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>-->
-
+  <div>
+    <Header style="position: fixed;z-index: 100;width: 100%;top: 0"></Header>
+<!--    <el-menu-->
+<!--      :default-active="activeIndex2"-->
+<!--      mode="horizontal"-->
+<!--      class="el-menu-demo menu"-->
+<!--      @select="handleSelect"-->
+<!--      background-color="#000000"-->
+<!--      text-color="#fff"-->
+<!--      active-text-color="#ffd04b">-->
+<!--      <el-menu-item index="1">Home</el-menu-item>-->
+<!--      <el-menu-item>-->
+<!--      <el-avatar  src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>-->
 <!--      </el-menu-item>-->
-
-<!--      <el-submenu index="2">-->
-<!--        <template slot="title">Workspace</template>-->
-<!--        <el-menu-item index="2-1">item one</el-menu-item>-->
-<!--        <el-menu-item index="2-2">item two</el-menu-item>-->
-<!--        <el-menu-item index="2-3">item three</el-menu-item>-->
-<!--        <el-submenu index="2-4">-->
-<!--          <template slot="title">item four</template>-->
-<!--          <el-menu-item index="2-4-1">item one</el-menu-item>-->
-<!--          <el-menu-item index="2-4-2">item two</el-menu-item>-->
-<!--          <el-menu-item index="2-4-3">item three</el-menu-item>-->
-<!--        </el-submenu>-->
-<!--      </el-submenu>-->
-<!--      <el-menu-item index="3" disabled>Info</el-menu-item>-->
-<!--      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>-->
-    </el-menu>
-    <Nuxt  />
+<!--    </el-menu>-->
+    <Nuxt/>
     <Footer></Footer>
   </div>
 
@@ -41,9 +22,10 @@
 
 <script>
 import Footer from "./Footer";
+import Header from "./Header";
 export default {
   components:{
-    Footer
+    Footer,Header
   },
   data() {
     return {
