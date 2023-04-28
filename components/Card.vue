dd362f2c2074366fd77a84b89ec728f7d5337275
phtchung <phamthanhchung2001@gmail.com>
2023-04-28 16:31:43 +0700

    access data

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="d-flex flex-column" >
-    <img :src=image style="width: 100%;background-size: cover;border-radius: 12px;transition: transform 0.3s ease-in-out; " alt="">
+  <div class="d-flex flex-column  " style="width: 100%" >
+    <img :src="'https://image.tmdb.org/t/p/original'+image"  :style="styleObj" style="width: 100%;background-size: cover;border-radius: 12px;transition: transform 0.3s ease-in-out;" alt="">
     <h4 class="title_name"> {{title}}</h4>
+    <h4 class="title_actor" v-if="name!==undefined" >{{name}}</h4>
   </div>
 </template>
 <script>
@@ -10,18 +11,25 @@ export default {
   props: {
     image:String,
     title:String,
+    name:String,
+    styleObj:String,
   },
   data(){
     return{
 
     }
+  },
+  mounted() {
+    console.log(this.name)
   }
 }
 </script>
 
 <style scoped>
 
+
 .title_name{
+  text-decoration: none;
   color: white ;
   text-align: center;
   margin: 1rem 0;
@@ -29,10 +37,20 @@ export default {
   cursor: pointer;
   font-size: 16px
 }
+.title_actor{
+  color: white;
+  text-align: center;
+  margin-top: -10px;
+  font-weight: 550;
+  font-size: 16px;
+}
 .title_name:hover{
   color: #E85F54;
 }
 
+img{
+  width: 100%;
+}
 img:hover{
   cursor: pointer;
 }
