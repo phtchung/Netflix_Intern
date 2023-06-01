8f2146ef4a8476ce685b5d999537e47631b9eb18
phtchung <phamthanhchung2001@gmail.com>
2023-06-01 12:39:14 +0700

    get api now playing

--- store/index.js
+++ store/index.js
@@ -3,19 +3,27 @@ const store = () => {
   return new Vuex.Store({
     state: {
       topfilms: [],
+      nowplayingfilms:[],
     },
     mutations: {
       setTopFilm(state, topfilms) {
         state.topfilms = topfilms
       },
-
+      setNowplayingFilm(state ,nowplayingfilms ){
+        state.nowplayingfilms = nowplayingfilms
+      }
     },
     actions: {
       async fetchData({ commit }) {
         const response = await this.$axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US&page=1')
         commit('setTopFilm', response.data.results)
         console.log(response.data.results)
-      }
+      },
+      async fetchNowPlayingData({ commit }) {
+        const response = await this.$axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US&page=1')
+        commit('setNowplayingFilm', response.data.results)
+        console.log(response.data.results)
+      },
     },
     getters: {
 
